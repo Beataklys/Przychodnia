1deda2ee9c5855b50c793f58587b3736d61ec188
Beataklys <beata.klyss@gmail.com>
2016-08-20 16:17:32 +0200

    third commit

--- app/controllers/irenas_controller.rb
+++ app/controllers/irenas_controller.rb
@@ -1,5 +1,5 @@
 class IrenasController < ApplicationController
-  before_action :set_doctor, only: [:show, :new, :patients]
+  before_action :set_doctor, only: [ :new, :patients]
   def check
 
     date_s = Date.today.to_s + " "+ "#{8+params[:hour].to_i}:00:00"
@@ -11,18 +11,24 @@ class IrenasController < ApplicationController
       redirect_to irena_path irena, doctor_id: params[:doctor_id]
     else
       #redirect_to new_irena_path doctor_id: params[:doctor_id]
-        redirect_to patients_irenas_path doctor_id: params[:doctor_id]
+        redirect_to patients_irenas_path doctor_id: params[:doctor_id], date: datetime
     end
   end
 
   def show
+    @irena= Irena.find(params[:id])
   end
   def new
     @irena = Irena.new
     @patients = Patient.where("name like?", "%#{params[:q]}%").where("pesel like ?", "%#{params[:p]}%")
   end
   def patients
-
+  end
+  def create
+  @irena =  Irena.create(doctor: Doctor.find(params[:doctor_id]),
+                          patient: Patient.find(params[:patient_id]),
+                          hour: DateTime.parse(params[:date]))
+            redirect_to @irena
   end
   private
   def set_doctor
